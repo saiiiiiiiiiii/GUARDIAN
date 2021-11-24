d22f5a16f45bd0845d26f59bdc00065396452ded
Boss Sai Kumar <mosurisaikumar@gmail.com>
2021-11-24 12:32:07 +0530

    hdudd

--- src/pages/LegitimacyProtocol/LegitimacyProtocol.js
+++ src/pages/LegitimacyProtocol/LegitimacyProtocol.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import "./LegitimacyProtocol.css"
 
+
 const LegitimacyProtocol = () => {
     return (
-        <div>
-            <h1>Legitimacy Protocol</h1>
-        </div>
+        <>
+        
+        <section className="section1" style={{paddingBottom:"60%"}}>
+        <div className="container">
+       
+      <video className="desk_vid" data-mask="70" playsinline="playsline" autoplay="autoplay" muted="muted" loop="loop">
+     
+      <source src="https://www.guardianlink.io/product-hotspot/video/product.mp4" type="video/mp4"/> 
+      </video>
+      <div className="overlay">
+        <h1>No-Code NFT Launchpad Platform</h1>
+        <p>Tap into the Multiverse of NFTs within minutes. Be your own NFT LaunchPad, tailored to your audience.</p>
+      </div>
+      </div>
+      </section>
+           
+        </>
     )
 }
 
--- src/pages/NoCodePlatform/NoCodePlatform.js
+++ src/pages/NoCodePlatform/NoCodePlatform.js
@@ -1,11 +1,185 @@
 import React from 'react'
 import "./NoCodePlatform.css"
 
+
+
 const NoCodePlatform = () => {
     return (
-        <div>
-            <h1>this is nocodeplatform</h1>
+      <>
+      <section className="section1" style={{paddingBottom:"60%"}}>
+        <div className="container">
+       
+      <video className="desk_vid" data-mask="70" playsinline="playsline" autoplay="autoplay" muted="muted" loop="loop">
+     
+      <source src="https://www.guardianlink.io/product-hotspot/video/product.mp4" type="video/mp4"/> 
+      </video>
+      <div className="overlay">
+        <h1>No-Code NFT Launchpad Platform</h1>
+        <p>Tap into the Multiverse of NFTs within minutes. Be your own NFT LaunchPad, tailored to your audience.</p>
+      </div>
+      </div>
+      </section>
+      <section className="section2"style={{paddingBottom:"70%"}}>
+      <h2>Experience the power of code. <br/>Without writing it.</h2>
+      <p>
+        Take control of your idea and let Guardianlink translate your vision into your own branded NFT LaunchPad, ready to scale up to serve Millions of collectors with high velocity trades.
+       </p>
+       <div >
+        <img src="https://www.guardianlink.io/product-hotspot/images/nft_5.png" alt="image "width="40%"/>
+         </div>
+       
+       
+      </section>
+      
+      <section className="section3"style={{paddingBottom:"15%"}}>
+      
+       <h2>Join the fastest growing ecosystem</h2>
+       <p>GuardianLink is the one of the fastest growing NFT ecosystem providers, with over 45 projects spanning DeFi, NFTs, Web3 and more.</p>
+       <div className="learnMore">
+         <a data-toggle="modal" data-target="#contact_us" href="">Work with us</a>
         </div>
+      </section>
+      
+      <section className="section4" >
+       
+        <h2>With our seamless minting API, your NFT marketplace can offer</h2>
+        <div className="row" >
+        <div className="block1"  >
+        <h6 >Anti-Rip <br clear="m-none"/> Dec_Sec </h6>
+        <p>GuardianLink burns in features into every NFT you mint on our platform that only allows the underlying media to be only accessed by its current owner. And this happens completely decentralized.</p>
+        </div>
+        <div className="block2"  >
+        <h6 >Fraud and Theft <br clear="m-none"/> Prevention</h6>
+        <p>We work with industry veterans to take custody of your NFTs and also use banking level scrutinies to keep your LaunchPad payments secure.</p>
+        </div>
+        <div className="block3" >
+        <h6 >Interoperable Provenance <br clear="m-none"/> Authentication</h6>
+        <p>The NFTs minted on our platform has decentralized features built into them that allow your collectors to port it to any marketplace or blockchain of their choice to find forward buyers.</p>
+        </div>
+        <div className="block4"  >
+        <h6 >Keep your <br clear="m-none"/> NFTs Unique</h6>
+        <p>Through our legitimacy protocol, we index and inventorise all the events emitted by major marketplaces, which means that if a copy of any of your NFTs pop up somewhere you will be notified immediately.</p>
+        </div>
+        <div className="block5"  >
+        <h6 >Fiat &amp; Crypto<br clear="m-none"/> NFTs for everyone</h6>
+        <p>While crypto folks are already knee deep in NFT frenzy, the LaunchPad you are able to launch through GuardianLink will help you also serve the uninitiated. Being able to accept credit/debit cards and bank transfers will open up a blue ocean for you.</p>
+        </div>
+        <div className="block6"  >
+        <h6 >Borderless <br clear="m-none"/> Royalties</h6>
+        <p>Our groundbreaking GuardLink™ smart contract allows for unbroken royalties across marketplaces and even blockchains.</p>
+        </div>
+        </div>
+    </section>
+    <section className="section5" style={{paddingBottom:"5%"}}>
+    
+    <h2>Strategic Partners</h2>
+     <p>We are grateful to our partners and advisors who have advised and guided us throughout our journey.</p>
+     <div className="row1">
+					
+						<div className="google">
+							<img src="https://www.guardianlink.io/product-hotspot/images/client/client_7.png" alt="img1"/>
+						
+					</div>
+				
+						<div className="oracle">
+							<img src="https://www.guardianlink.io/product-hotspot/images/client/client_6.png" alt="img2"/>
+						
+					</div>
+                    
+						<div className="econet">
+							<img src="https://www.guardianlink.io/product-hotspot/images/client/client_1.png" alt="img3"/>
+						
+					</div>
+				</div>
+
+
+        <div className="row2">
+                
+					
+						<div className="ibm">
+							<img src="https://www.guardianlink.io/product-hotspot/images/client/client_3.png" alt="img1"/>
+						
+					</div>
+					
+						<div className="amazon">
+							<img src="https://www.guardianlink.io/product-hotspot/images/client/client_4.png" alt="img2"/>
+						
+					</div>
+					
+					
+						<div className="globant">
+							<img src="https://www.guardianlink.io/product-hotspot/images/client/client_5.png" alt="img3"/>
+						
+					</div>
+					
+						<div className="sportify">
+							<img src="https://www.guardianlink.io/product-hotspot/images/client/client_2.png" alt="img4"/>
+						
+					</div>
+                </div>
+
+    </section>
+    <section className="section6" style={{paddingBottom:"10%"}}>
+    <div className="container">
+     <h2>MINT.<br/>
+       Launch across Marketplaces &amp; Blockchains</h2>
+      <p>Mint NFTs, enable royalties, and list in any open marketplace or blockchain, all of that with absolutely no coding.</p><br/>
+      <p>Your collectors can port NFTs you minted to any other marketplace or Blockchain, we have built-in features that will help prove provenance and keep your royalty percentages intact.</p><br/>
+     <p>Right now NFT project list the author wallet address in the original published blockchain alone so that collectors can be mindful. But what happens when your collector wants to take it elsewhere. Your brandable LaunchPad has this challenge solved already. </p>
+     
+      <img src="https://www.guardianlink.io/product-hotspot/images/nft_6.png" alt="img1 " width="100%"/>
+        
+         </div>
+    </section>
+    <section className="section7" style={{paddingBottom:"35%"}}>
+    <div className="container">
+                        <div className="sai">
+                           
+                                <h2>GuardianLink Protocol Feature Set</h2>
+                                <p>Our features range from protecting not only the art and the artist but also the collectors.</p>
+                            
+                        </div>
+                        <div className="container1">
+                            <div className="image1">
+                              <img src="https://www.guardianlink.io/product-hotspot/images/node.png"/>
+                              <h6>Anti Rip - DEC_SEC</h6>
+                              <p>The most important aspect of art for both artists and collectors is for the value to be unique. Which is where our Anti-Rip Protocol kicks into action as decentralized security built-in to NFTs we mint.</p>
+                             </div>
+                          <div className="image2">
+                             <img src="https://www.guardianlink.io/product-hotspot/images/marketing.png"/>
+                                <h6 className="feature-title">Market Manipulation</h6>
+                                <p>Just like Greco in Oceans 13, our platform will crawl and index all the events emitted by major marketplaces, while determining if any price movement is either organic or manufactured.</p>
+                          </div>
+                            <div className="image3">
+                            <img src="https://www.guardianlink.io/product-hotspot/images/royalties.png"/>
+                              <h6 className="feature-title">Royalties Across Borders</h6>
+                              <p>For artists, we provide a one stop solution to enforce their NFT royalties across marketplaces and for that matter, even across blockchains. Now your collectors can truly be free to find forward buyers wherever they want.</p>
+                             </div>
+                                <div className="image4">
+                                  <img src="https://www.guardianlink.io/product-hotspot/images/stock-market.png"/>
+                                   <h6 className="feature-title">Trading View but for NFTs</h6>
+                                    <p>When we identify a whale or institution buying an NFT in a nondescript way, we then take a deeper look at that collection so that we beat the FOMO before anyone else.</p>
+                                   </div>
+                                        <div className="image5">
+                                            <img src="https://www.guardianlink.io/product-hotspot/images/data-recovery.png"/>
+                                        <h6 className="feature-title">Recoverability</h6>
+                                        <p>When a decentralized governance protocol that includes collectors and the artist decide democratically, we initiate doomsday protocol to recover an NFT completely.</p>
+                                    </div>
+                                    
+                                    
+                                        <div className="image6">
+                                            <img src="https://www.guardianlink.io/product-hotspot/images/warning.png"/>
+                                        <h6 className="feature-title">Dupe Alert</h6>
+                                        <p>Think of Google on steroids for the NFT universe. As soon as any one of your NFTs finds itself a dopple ganger in any marketplace, you will be the first to know, to take action in anyway you see fit.</p>
+                                    </div>
+                                  
+                                </div>
+                            </div>
+                        
+                    
+    </section>
+    
+        </>
     )
 }
 
